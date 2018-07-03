28cff1a4ed5ce67c83bc5c2d911441873f6c602e
Hironobu Saitoh <hiro@hironobu.org>
2018-07-03 16:07:48 +0900

    scssを扱えるようにした

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,64 @@
 var path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    mode: "development",
-    entry: {
-	popup:['./src/js/conohawp.js', './src/js/popup.js'],
-	content: ['./src/js/conohawp.js', './src/js/content.js'],
-	background:'./src/js/background.js'
-    },
-    output: {
-	path: path.join(__dirname, 'ext'),
-	filename: '[name].js',
-    },
-    devtool: "inline-source-map",
-    module: {
-	rules: [
-	    { 
-		test: /\.js$/, 
-		exclude: /node_modules/, 
-		loader: "babel-loader", 
-		query:{
-		    presets: ['es2015']
+const MODE = "development"
+
+module.exports = [
+    {
+	mode: MODE,
+	entry: {
+	    popup:['./src/js/conohawp.js', './src/js/popup.js'],
+	    content: ['./src/js/conohawp.js', './src/js/content.js'],
+	    background:'./src/js/background.js',
+	},
+	output: {
+	    path: path.join(__dirname, 'ext'),
+	    filename: '[name].js',
+	},
+	devtool: "inline-source-map",
+	module: {
+	    rules: [
+		{ 
+		    test: /\.js$/, 
+		    exclude: /node_modules/, 
+		    loader: "babel-loader", 
+		    query:{
+			presets: ['es2015']
+		    }
 		}
-	    }
-	]
+	    ]
+	},
+    },
+    {
+	mode: MODE,
+	entry: {
+	    popup:'./src/scss/popup.scss',
+	},
+	output: {
+	    path: path.join(__dirname, 'ext'),
+	    filename: '[name].css',
+	},
+	module:{
+	    rules: [
+		{
+		    test: /\.scss$/,
+		    use: ExtractTextPlugin.extract({
+			fallback: 'style-loader',
+			use: [
+			    {
+				loader: 'css-loader',
+				options: {url: false}
+			    },
+			    {
+				loader: 'sass-loader'
+			    }
+			]
+		    })
+		}
+	    ]
+	},
+	plugins: [
+	    new ExtractTextPlugin('[name].css')
+	]	
     }
-};
+];

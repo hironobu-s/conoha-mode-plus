30b6e4cd3e546e496d054e20ab67e5adbc503ef3
Hironobu Saitoh <hiro@hironobu.org>
2018-07-05 11:55:54 +0900

    ページ読み込み時にオリジナルの壁紙を表示しないようにした

--- src/js/content.js
+++ src/js/content.js
@@ -6,7 +6,7 @@ const replace = function(url) {
 
     // replace wallpaper
     const bg = document.querySelector(".bg-all");
-    bg.style.backgroundImage = "url(" + url + ")";
+    bg.setAttribute('style', "background-image: url(" + url + ") !important");
 }
 
 // Enable page action button and add event handler 
@@ -16,9 +16,19 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     replace(msg.url);
 });
 
-// Restore wallpapre
-const url = localStorage.getItem("wp_image_url");
-if(url != null) {
-    replace(url)
-}
+// Add inline CSS clearing original background image
+var s = document.createElement ("style");
+var rule = document.createTextNode('.bg-all { background-image: none !important; }');
+s.media = 'screen';
+s.type = 'text/css';
+s.appendChild(rule);
+document.documentElement.appendChild(s);
+
+// Restore wallpapre after DOM loaded
+document.addEventListener("DOMContentLoaded", function(e) {
+    const url = localStorage.getItem("wp_image_url");
+    if(url != null) {
+	replace(url)
+    }
+});
 

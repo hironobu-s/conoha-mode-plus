921ba8f8cc6042b7525e0052c47f29daa9433fc3
Hironobu Saitoh <hiro@hironobu.org>
2019-07-10 18:13:44 +0900

    壁紙変更

--- src/js/conohawp.js
+++ src/js/conohawp.js
@@ -1,57 +1,57 @@
-module.exports.types = function() {
-    return [
-	"2018_autumn",
-	"2018_swim",
-	"2018_yukata",
-	"2018_rain",
-	"2018_spring",
-	"2018_newyear",
-	"2017_winter",
-	"2017_christmas",
-	"2017_halloween",
-	"2017_autumn",
-	"2017_summer",
-	"2017_spring",
-	"2017_newyear",
-	"2016_christmas",
-	"2016_skiing",
-	"2016_tachi",
-	"2016_halloween",
-	"2016_autumn",
-	"2016_hiyoko",
-	"2016_comic",
-	"2016_summer",
-	"2016_spring",
-	"2016_pop",
-	"2015_pop",
-	"2015_osc",
-	"2015_summer",
-	"2015_autumn",
-	"2015_xmas",
-    ];
+module.exports.types = function () {
+	return [
+		"2019_summer",
+		"2019_spring",
+		"2019_newyear",
+		"2018_christmas",
+		"2018_winter",
+		"2018_autumn",
+		"2018_swim",
+		"2018_yukata",
+		"2018_rain",
+		"2018_spring",
+		"2018_newyear",
+		"2017_winter",
+		"2017_christmas",
+		"2017_halloween",
+		"2017_autumn",
+		"2017_summer",
+		"2017_spring",
+		"2017_newyear",
+		"2016_christmas",
+		"2016_skiing",
+		"2016_tachi",
+		"2016_halloween",
+		"2016_autumn",
+		"2016_hiyoko",
+		"2016_comic",
+		"2016_summer",
+		"2016_spring",
+		"2016_pop",
+		"2015_pop",
+		"2015_osc",
+		"2015_summer",
+		"2015_autumn",
+		"2015_xmas",
+	];
 }
 
-module.exports.base_url = function() {
-    return "https://conoha.mikumo.com/wp-content/themes/conohamikumo/images/wallpaper/";
+module.exports.base_url = function () {
+	return "https://conoha.mikumo.com/wp-content/themes/conohamikumo/images/wallpaper/";
 }
 
-module.exports.image_url = function(type, screen_width) {
-    if(window.devicePixelRatio == 2) {
-	screen_width *= 2
-    }
-    
-    let file;
-    if(screen_width >= 1920) {
-	file = "1920_1080.jpg";
-    } else {
-	file = "1280_800.jpg";
-    }
-    
-    return this.base_url() + type + "/" + file;
+module.exports.image_url = function (type, screen_width) {
+	if (window.devicePixelRatio == 2) {
+		screen_width *= 2
+	}
+
+	const file = "1280_800.jpg";
+
+	return this.base_url() + type + "/" + file;
 }
 
-module.exports.thumb_url = function(type) {
-    return this.base_url() + type + "/thumb.jpg";
+module.exports.thumb_url = function (type) {
+	return this.base_url() + type + "/thumb.jpg";
 }
 
 
